c8b7529421e171378aab39b6dfcd26171d37859c
Muhammad Tariq <m.tariq@iub.edu.pk>
2024-05-15 00:34:58 +0500

    Menu, Role, Pivot, User Columns, and User Log implementation.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MenuController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,8 +19,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('admin.dashboard');
-});
+})->name('dashboard');
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/users/profile', [UserController::class, 'profile'])->name('users.profile');
+Route::resource('/users', UserController::class);
+Route::resource('/menus', MenuController::class);
+Route::resource('/roles', RoleController::class);
