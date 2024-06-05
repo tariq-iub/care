de00e7fd12bcc253e7d40bdb032b52f091bab24e
Muhammad Tariq <m.tariq@iub.edu.pk>
2024-06-05 12:55:44 +0500

    Url column removed from menus migration

--- database/migrations/2024_05_14_130743_create_menus_table.php
+++ database/migrations/2024_05_14_130743_create_menus_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->string('title')->unique();
             $table->string('icon')->nullable();
-            $table->string('url')->nullable();
             $table->string('route')->nullable();
             $table->unsignedBigInteger('parent_id')->nullable();
             $table->integer('display_order')->default(0);
