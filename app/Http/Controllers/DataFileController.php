5d6e8508459251b6823a2a441a75c3232aac9af4
Abdullah Abid <abdrps2004@gmail.com>
2024-12-17 11:14:00 +0500

    Update the data files table

--- app/Http/Controllers/DataFileController.php
+++ app/Http/Controllers/DataFileController.php
@@ -21,6 +21,7 @@ class DataFileController extends Controller
     {
         $plants = Plant::all();
         $devices = Device::all();
+
         return view('admin.data.index', compact('plants', 'devices'));
     }
 
@@ -31,6 +32,7 @@ class DataFileController extends Controller
     {
         $factories = Factory::all();
         $devices = Device::all();
+
         return view('admin.files.create', compact('factories', 'devices'));
     }
 
