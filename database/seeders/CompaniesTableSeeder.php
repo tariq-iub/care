a2caccff03ccfb3227429bda90e71f68f97045fd
Abdullah Abid <abdrps2004@gmail.com>
2024-12-21 00:11:24 +0500

    Updated the seeders for Companies -> Plants -> Areas AND Created the seeders for Machines -> VibrationLocations, Machines -> ProcessPoints.

--- database/seeders/CompaniesTableSeeder.php
+++ database/seeders/CompaniesTableSeeder.php
@@ -14,13 +14,13 @@ class CompaniesTableSeeder extends Seeder
      */
     public function run(): void
     {
-        Company::factory(10)->create()->each(function ($company) {
+        Company::factory(15)->create()->each(function ($company) {
             // For each company, create 10 plants
-            Plant::factory(5)->create([
+            Plant::factory(10)->create([
                 'company_id' => $company->id,
             ])->each(function ($plant) {
                 // For each plant, create 10 areas
-                Area::factory(25)->create([
+                Area::factory(5)->create([
                     'plant_id' => $plant->id,
                 ]);
             });
