f7db4df6683e96d01996dfb6ca32c6fa7722815e
Abdullah Abid <abdrps2004@gmail.com>
2025-01-07 10:51:43 +0500

    Working on machine hierarchy

--- app/Http/Controllers/MachineHierarchyController.php
+++ app/Http/Controllers/MachineHierarchyController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Company;
 use Illuminate\Http\Request;
 
 class MachineHierarchyController extends Controller
 {
-    //
+    /**
+     * Display the hierarchy view with all data.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function index()
+    {
+        // Fetch companies with their full hierarchy using Eager Loading
+        $companies = Company::with([
+            'plants',                      // Load plants for each company
+            'plants.areas',                // Load areas for each plant
+            'plants.areas.machines',       // Load machines for each area
+            'plants.areas.machines.machinePoints' // Load machine points for each machine
+        ])->get();
+
+        // Return the view with the fetched data
+        return view('admin.machine_hierarchy.index', compact('companies'));
+    }
 }
--- resources/views/admin/machine_hierarchy/index.blade.php
+++ resources/views/admin/machine_hierarchy/index.blade.php
@@ -1,1 +1,110 @@
-<?php
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>Hierarchy</h1>
+        @foreach($companies as $company)
+            <div class="mb-3">
+                <table class="table table-bordered">
+                    <thead>
+                    <tr>
+                        <th>Company: {{ $company->name }}</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>
+                            <button class="btn btn-link" data-bs-toggle="collapse" data-bs-target="#plants-{{ $company->id }}">
+                                View Plants
+                            </button>
+                            <div id="plants-{{ $company->id }}" class="collapse">
+                                @foreach($company->plants as $plant)
+                                    <table class="table table-bordered mt-2">
+                                        <thead>
+                                        <tr>
+                                            <th>Plant: {{ $plant->name }}</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        <tr>
+                                            <td>
+                                                <button class="btn btn-link" data-bs-toggle="collapse" data-bs-target="#areas-{{ $plant->id }}">
+                                                    View Areas
+                                                </button>
+                                                <div id="areas-{{ $plant->id }}" class="collapse">
+                                                    @foreach($plant->areas as $area)
+                                                        <table class="table table-bordered mt-2">
+                                                            <thead>
+                                                            <tr>
+                                                                <th>Area: {{ $area->name }}</th>
+                                                            </tr>
+                                                            </thead>
+                                                            <tbody>
+                                                            <tr>
+                                                                <td>
+                                                                    <button class="btn btn-link" data-bs-toggle="collapse" data-bs-target="#machines-{{ $area->id }}">
+                                                                        View Machines
+                                                                    </button>
+                                                                    <div id="machines-{{ $area->id }}" class="collapse">
+                                                                        @foreach($area->machines as $machine)
+                                                                            <table class="table table-bordered mt-2">
+                                                                                <thead>
+                                                                                <tr>
+                                                                                    <th>Machine: {{ $machine->name }}</th>
+                                                                                    <th>
+                                                                                        <button class="btn btn-primary">Start Diagnosis</button>
+                                                                                    </th>
+                                                                                </tr>
+                                                                                </thead>
+                                                                                <tbody>
+                                                                                <tr>
+                                                                                    <td colspan="2">
+                                                                                        <button class="btn btn-link" data-bs-toggle="collapse" data-bs-target="#points-{{ $machine->id }}">
+                                                                                            View Machine Points
+                                                                                        </button>
+                                                                                        <div id="points-{{ $machine->id }}" class="collapse">
+                                                                                            <table class="table table-bordered mt-2">
+                                                                                                <thead>
+                                                                                                <tr>
+                                                                                                    <th>Point</th>
+                                                                                                    <th>Action</th>
+                                                                                                </tr>
+                                                                                                </thead>
+                                                                                                <tbody>
+                                                                                                @foreach($machine->machinePoints as $point)
+                                                                                                    <tr>
+                                                                                                        <td>{{ $point->name }}</td>
+                                                                                                        <td>
+                                                                                                            <button class="btn btn-primary">Start Diagnosis</button>
+                                                                                                        </td>
+                                                                                                    </tr>
+                                                                                                @endforeach
+                                                                                                </tbody>
+                                                                                            </table>
+                                                                                        </div>
+                                                                                    </td>
+                                                                                </tr>
+                                                                                </tbody>
+                                                                            </table>
+                                                                        @endforeach
+                                                                    </div>
+                                                                </td>
+                                                            </tr>
+                                                            </tbody>
+                                                        </table>
+                                                    @endforeach
+                                                </div>
+                                            </td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                @endforeach
+                            </div>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        @endforeach
+    </div>
+@endsection
